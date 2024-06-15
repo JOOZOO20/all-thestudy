2459019e79b853a6be0b102df1abaa53bfefd930
Heeju <62732461+JOOZOO20@users.noreply.github.com>
2024-06-15 23:51:30 +0900

    두개의 정수를 받아서 두개의 정수를 곱한 값을 "출력"하는 다음 프로그램
    두개의 정수를 받아서 두개의 정수를 곱한 값을 "출력"하는 다음 프로그램을 적절한 함수형 인터페이스와 람다식을 사용하여 작성한 과제

--- S.C.H/2024-1/progressed-java/LamdaTest.java
+++ S.C.H/2024-1/progressed-java/LamdaTest.java
@@ -1,33 +1,32 @@
 //***************************
 
 // 파일명: LamdaTest .java
-
-// 작성일: 24.04.03
-
-// 내용: timer 클래스를 사용하여 1초에 한번씩 beep을 출력하는 기존 과제에 제시된 타이머 프로그램을 람다식을 이용하여 간결하게 표현하기.
-
+// 내용: 두개의 정수를 받아서 두개의 정수를 곱한 값을 "출력"하는 프로그램을 적절한 함수형 인터페이스와 람다식을 사용하여 작성하는 hw10_2 과제
 //***************************
 
-import java.awt.event.ActionEvent; 
-import javax.swing.Timer; 
+package hw10_2;
+
+import java.util.Scanner;
+import java.util.function.BiConsumer;
 
 public class LamdaTest {
+
     public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        int x;
+        int y;
 
-    	// timer객체를 생성하여 1초마다 beep이 출력되도록 하는 작동을 람다식으로 표현함.
-        Timer t = new Timer(1000, (ActionEvent event) -> {
-            System.out.println("beep"); 
-        });
+        // BiConsumer라는 함수형 인터페이스와 람다식을 사용하여 연산을 처리함.
+        BiConsumer<Integer, Integer> m = (a, b) -> System.out.println("\n두 정수의 곱은 " + (a * b) + " 입니다.");
 
-        t.start(); 
+        
+        System.out.print("첫 번째 정수 입력 : ");
+        x = sc.nextInt();
 
-        // 1초의 간격이 있도록 하는 반복문 (기존 과제에 올려진 코드와 동일함)
-        for (int i = 0; i < 1000; i++) {
-            try {
-                Thread.sleep(1000); //1초 후에 출력되도록 sleep을 사용함.
-            } catch (InterruptedException e) {
-                
-            }
-        }
+        System.out.print("두 번째 정수 입력 : ");
+        y = sc.nextInt();
+
+        m.accept(x, y);
     }
 }
