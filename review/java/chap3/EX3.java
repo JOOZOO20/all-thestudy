7b96357b0660cfcd5168974b52c32d852cd27205
JOOZOO20 <chaheeju01@gmail.com>
2024-11-22 14:41:52 +0900

    자바의 정석 기초편 chapter3 복습

---
+++ review/java/chap3/EX3.java
@@ -0,0 +1,15 @@
+public class EX3 {
+    public static void main(String args[]){
+
+//        System.out.println(3&&4<5);
+
+/* <위의 문장을 실행한 이유: 3이라는 숫자를 true로 인식할까에 대한 궁금함이 있었음. 마치 c,python에선 0이외의 숫자를 boolean형으로 바꿔 true로 인식하듯이!>
+   [결과 : 자바는 true로 인식하지 않고, 그저 int는 int로 바라보며, 아래와 같은 에러를 띄움.]
+ java: bad operand types for binary operator '&&'
+  first type:  int
+  second type: boolean
+*/
+
+
+    }
+}
