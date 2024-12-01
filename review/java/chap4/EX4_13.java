c8254574567d79998a761b0d215470738bed9d0d
Heeju <62732461+JOOZOO20@users.noreply.github.com>
2024-12-01 23:51:21 +0900

    자바의 정석 기초편 chapter4 복습

---
+++ review/java/chap4/EX4_13.java
@@ -0,0 +1,13 @@
+package chap4;
+
+public class EX4_13 {
+    public static void main(String args[]){
+        int sum=0;
+        int i=0;
+
+        while(sum<=100){
+            System.out.printf("%d - %d\n",i,sum);
+            sum+=++i;
+        }
+    }
+}
