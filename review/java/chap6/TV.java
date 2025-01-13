051f431df8d7f856ac551f23d3eda4b815b83972
Heeju <62732461+JOOZOO20@users.noreply.github.com>
2025-01-13 12:49:37 +0900

    자바의 정석 기초편 chapter6 복습
    '객체의 생성과 사용' 내용 복습

---
+++ review/java/chap6/TV.java
@@ -0,0 +1,11 @@
+package chap6;
+
+public class TV{
+    String color;
+    boolean power;
+    int channel;
+
+    void power() {power =!power;}
+    void channelUp() {++channel;}
+    void channelDown(){--channel;}
+}
