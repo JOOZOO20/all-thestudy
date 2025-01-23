44e5ad4b7fe99452cfef5f3a8cec6ef27d669fcb
Heeju <62732461+JOOZOO20@users.noreply.github.com>
2025-01-23 09:18:07 +0900

    자바의 정석 기초편 'chap6_ 생성자에서 다른 생성자 호출하기_this() ' 복습
    메인 클래스

---
+++ review/java/chap6/Ex6_13.java
@@ -0,0 +1,11 @@
+package chap6;
+
+public class Ex6_13 {
+    public static void main(String args[]){
+        Car2 c1 = new Car2();
+        Car2 c2 = new Car2();
+
+        System.out.println("c1의 색상 = "+c1.color+", 기어 타입 = "+c1.gearType+", 문의 갯수 = "+c1.door);
+        System.out.println("c2의 색상 = "+c2.color+", 기어 타입 = "+c2.gearType+", 문의 갯수 = "+c2.door);
+    }
+}
