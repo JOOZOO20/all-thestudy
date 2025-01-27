ab19e797601a388e7bd9f30da81bb63ec8202522
Heeju <62732461+JOOZOO20@users.noreply.github.com>
2025-01-28 00:04:47 +0900

    자바의 정석 기초편 'ch7_참조변수의 형변환' 내용 복습
    조상클래스인 'Car' 클래스

---
+++ review/java/chap7/Car.java
@@ -0,0 +1,13 @@
+package chap7;
+
+public class Car {
+    String color;
+    int door;
+
+    void drive(){
+        System.out.println("drive, Brrr~");
+    }
+    void stop(){
+        System.out.println("stop!!");
+    }
+}
