d4ec0144e101e516b847ff3f1b2864d3aeb3027a
Heeju <62732461+JOOZOO20@users.noreply.github.com>
2025-02-02 12:52:21 +0900

    자바의 정석 기초편 'ch7_디폴트 메서드와 static 메서드' 개념 학습
    메인 클래스

---
+++ review/java/chap7/Ex7_11.java
@@ -0,0 +1,12 @@
+package chap7;
+
+public class Ex7_11 {
+    public static void main(String args[]){
+        Child3 c = new Child3();
+        c.method1();
+        c.method2();
+        MyInterface.staticMethod();
+        MyInterface2.staticMethod();
+    }
+
+}
